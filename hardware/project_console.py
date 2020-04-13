43470a11960f52dc9b7e62792d41833df5811589
andresdavalosc <andres_el_davalos@hotmail.com>
2020-04-13 21:19:06 +0200

    Add files via upload

--- hardware/project_console.py
+++ hardware/project_console.py
@@ -6,11 +6,15 @@ import paho.mqtt.publish as publish
 import sys
 from time import sleep
 
+val=0
 led = [10,9,11,5,6,13,19]
+virus = 22
+wcrol = 27
+car  = 17
 prevcount = 0
-ab = False
+#ab = False
 count = 1
-start = 0
+start = False
 begin = True
 knop1,knop2=3,2
 GPIO.setmode(GPIO.BCM)
@@ -18,31 +22,57 @@ GPIO.setup(knop1,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(knop2,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 for x in led:
     GPIO.setup(x,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(virus,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(wcrol,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(car,GPIO.OUT,initial=GPIO.LOW)
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("Desktop/project")
+
+
 def Up(a):
     if GPIO.event_detected(knop1):
-        global begin
+        global begin,val,count
         if begin == True:
             begin = False
+            publish.single("Desktop/project",payload= count, hostname="anonymous10.ddns.net")
+        elif start == True:
+            print("Up")
+            #val +=1
+            publish.single("Desktop/project",payload= "UP" , hostname="anonymous10.ddns.net")
         sleep(1)
+
 def Dn(a):
     if GPIO.event_detected(knop2):
-        global begin
+        global begin,val
         if begin == True:
             global count
             #print("gedrukt")
             count+=1
             if count > 3:
                 count = 1;
+        elif start == True:
+            print("Dn")
+            #val -=1
+            publish.single("Desktop/project",payload= "DN", hostname="anonymous10.ddns.net")
         sleep(1)
 
+
 def nummereen():
+  GPIO.output(wcrol,GPIO.LOW)
+  GPIO.output(car,GPIO.LOW)
+  GPIO.output(virus,GPIO.HIGH)
   GPIO.output(led[0],GPIO.LOW)
   GPIO.output(led[1],GPIO.HIGH)
   GPIO.output(led[2],GPIO.HIGH)
   GPIO.output(led[3],GPIO.LOW)
   GPIO.output(led[6],GPIO.LOW)
   GPIO.output(led[4],GPIO.LOW)
+
 def nummertwee():
+  GPIO.output(virus,GPIO.LOW)
+  GPIO.output(car,GPIO.LOW)
+  GPIO.output(wcrol,GPIO.HIGH)
   GPIO.output(led[0],GPIO.HIGH)
   GPIO.output(led[1],GPIO.HIGH)
   GPIO.output(led[3],GPIO.HIGH)
@@ -51,6 +81,9 @@ def nummertwee():
   GPIO.output(led[2],GPIO.LOW)
 
 def nummerdrie():
+  GPIO.output(virus,GPIO.LOW)
+  GPIO.output(wcrol,GPIO.LOW)
+  GPIO.output(car,GPIO.HIGH)
   GPIO.output(led[0],GPIO.HIGH)
   GPIO.output(led[1],GPIO.HIGH)
   GPIO.output(led[2],GPIO.HIGH)
@@ -63,12 +96,16 @@ try:
     os.system('clear')
     GPIO.add_event_detect(knop1,GPIO.RISING,callback=Up)
     GPIO.add_event_detect(knop2,GPIO.RISING,callback=Dn)
-    print("""welcome to the..
-                             ~~~~~CORONA GAME~~~~~"""
-                                )
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect(host="anonymous10.ddns.net")
+    print("""welcome to the
+                             ~~~~~CORONA GAME~~~~~
+                                """)
     print("""choose your character with the up button, PRESS down to select
            (1)Virus  (2)toilet paper (3)shopping Car
                                                      """)
+
     #begin = True
     while begin == True:
        if count == prevcount:
@@ -85,6 +122,10 @@ try:
            sleep(1)
 
     print("start the game.. You chose "+ str(count))
+    while 1:
+        start = True
+
+    client.loop_forever()
 except KeyboardInterrupt:
     print("close")
 finally:
