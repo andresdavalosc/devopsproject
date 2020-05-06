1d20b433bfb845b6c136e29986321dd2571ececa
Ajon <ajonsubedi@gmail.com>
2020-05-06 14:47:02 +0200

    score counter

--- software/tkinter/controleVenster.py
+++ software/tkinter/controleVenster.py
@@ -1,5 +1,20 @@
+
 import tkinter as tk
 from time import sleep
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+
+counter = 0
+def on_message(client, userdata, msg):
+      global counter
+      print(str(msg.payload))
+      counter += 1
+      print(counter)
+
+client = mqtt.Client()
+client.on_message = on_message
+client.connect(host="anonymous10.ddns.net")
+client.subscribe("Desktop/score")
+client.loop_forever()
+
 
