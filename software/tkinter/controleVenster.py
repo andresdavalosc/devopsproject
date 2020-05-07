927f2c6e59c45f1f4d09b8bb29362aa320e3212b
Ajon <ajonsubedi@gmail.com>
2020-05-07 16:10:41 +0200

    Timer is gemaakt in de controle venster

--- software/tkinter/controleVenster.py
+++ software/tkinter/controleVenster.py
@@ -1,16 +1,27 @@
 
 import tkinter as tk
-from time import sleep
+import time
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
-counter = 0
+score = 0
 def on_message(client, userdata, msg):
-      global counter
+      global score
       print(str(msg.payload))
-      counter += 1
-      print(counter)
+      score += 1
+      print(score)
 
+def timer(count):
+      for i in range(count, -1, -1):
+           m, s = divmod(i, 60)
+           h, m = divmod(m, 60)
+           time_left = str(h).zfill(2) + ":" + str(m).zfill(2) + ":" + str(s).zfill(2)
+           print(time_left)
+           time.sleep(1)
+           if i == 0:
+                print("Game over!")
+
+timer(10)
 client = mqtt.Client()
 client.on_message = on_message
 client.connect(host="anonymous10.ddns.net")
