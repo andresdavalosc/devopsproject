060530bb90d71053121eed58b0c477173bfd2562
ElMoufid-Mohamed <elmoufid.mohamed@outlook.com>
2020-05-21 18:33:06 +0200

    Add Basic OOP Movement & Refactor Class

--- software/tkinter/new.py
+++ software/tkinter/new.py
@@ -1,3 +1,4 @@
+import threading
 import tkinter as tk
 from time import sleep
 import paho.mqtt.client as mqtt
@@ -6,22 +7,26 @@ import tkinter.font as tkFont
 
 class Object:
 
-	def __init__(self, master = None, kader = None, x = None, y = None, img = None):
+	def __init__(self, master = None, canvas = None, xpos = None, ypos = None, tkphoto = None):
 		self.master = master
-		self.kader = kader
-		self.x = x
-		self.y = y
-		self.created_img = self.Create(x, y, img)
+		#self.kader = kader
+		self.xpos = xpos
+		self.ypos = ypos
+		self.created_img = self.Create(canvas, xpos, ypos, tkphoto)
 
-	def Create(self, x, y, img):
-		return self.kader.create_image(x, y, anchor = tk.NW, image=img)
+	def Create(self, canvas, xpos, ypos, tkphoto):
+		created_img = canvas.create_image(xpos, ypos, anchor = tk.NW, image=tkphoto)
+		return created_img
+
+	def Move(self, canvas):
+		canvas.move(self.created_img, 2, 2)
 
 class Game:
 
 	def __init__(self, master = None):
 		self.master = master
 
-	def Main1(self):
+	def Start(self):
 		width = 1920
 		height = 990
 
@@ -29,6 +34,8 @@ class Game:
 		tekst.pack()
 		kader = tk.Canvas(master, width=width, height=height, background="black")
 		kader.pack()
+
+		# Create Game Objects
 
 		self.virus = tk.PhotoImage(file="./img/virus.png")
 		self.cart = tk.PhotoImage(file="./img/cart.png")
@@ -38,8 +45,21 @@ class Game:
 		Rol = Object(master, kader, 0, 180, self.rol)
 		Cart = Object(master, kader, 0, 280, self.cart)
 
+		allObjects = [Virus, Rol, Cart]
+
+		x = threading.Thread(target=self.loop, args=(kader, allObjects))
+		x.start()
+
+	def loop(self, kader, allObjects):
+		while True:
+			for obj in allObjects:
+				obj.Move(kader)
+
+			sleep(0.25)
+
+
 if __name__ == "__main__":
-	master = tk.Tk() 
+	master = tk.Tk()
 	game = Game(master)
-	game.Main1()
+	game.Start()
 	tk.mainloop()
